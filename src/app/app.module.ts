03a0331dc43602d621c80f9bd61bb974de26c0ef
saifmathur <saifmathur41@gmail.com>
2023-10-20 20:30:30 +0530

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { InputTextModule } from "primeng/inputtext";
 import { DropdownModule } from "primeng/dropdown";
 import { SidebarModule } from "primeng/sidebar";
+
 import { ToastModule } from "primeng/toast";
 import { BreadcrumbModule } from "primeng/breadcrumb";
 import { TableModule } from "primeng/table";
 import { CheckboxModule } from "primeng/checkbox";
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -31,6 +34,9 @@ import { CheckboxModule } from "primeng/checkbox";
     BreadcrumbModule,
     TableModule,
     CheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent],
