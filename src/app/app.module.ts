41668b32dc33e2c1cc055bb10dc489ca0f43795d
saifmathur <saifmathur41@gmail.com>
2023-10-20 20:51:40 +0530

    cart icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,22 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { ButtonModule } from "primeng/button";
-import { CardModule } from "primeng/card";
-import { InputTextModule } from "primeng/inputtext";
-import { DropdownModule } from "primeng/dropdown";
-import { SidebarModule } from "primeng/sidebar";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatMenuModule } from "@angular/material/menu";
 
-import { ToastModule } from "primeng/toast";
-import { BreadcrumbModule } from "primeng/breadcrumb";
-import { TableModule } from "primeng/table";
-import { CheckboxModule } from "primeng/checkbox";
+
 import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
-    BrowserModule,
+    BrowserModule,MatBadgeModule,
     AppRoutingModule,
+    MatIconModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
-    ButtonModule,
-    CardModule,
-    InputTextModule,
-    DropdownModule,
-    SidebarModule,
-    ToastModule,
-    BreadcrumbModule,
-    TableModule,
-    CheckboxModule,
+    MatSidenavModule,MatMenuModule,
     MatSidenavModule,
     MatToolbarModule,
   ],
