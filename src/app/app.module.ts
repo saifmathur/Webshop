66c9085083333ead13a31eb6946ed8dbda16d822
saifmathur <saifmathur41@gmail.com>
2023-10-20 20:12:33 +0530

    added primeng theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { ButtonModule } from "primeng/button";
+import { CardModule } from "primeng/card";
+import { InputTextModule } from "primeng/inputtext";
+import { DropdownModule } from "primeng/dropdown";
+import { SidebarModule } from "primeng/sidebar";
+import { ToastModule } from "primeng/toast";
+import { BreadcrumbModule } from "primeng/breadcrumb";
+import { TableModule } from "primeng/table";
+import { CheckboxModule } from "primeng/checkbox";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    DropdownModule,
+    SidebarModule,
+    ToastModule,
+    BreadcrumbModule,
+    TableModule,
+    CheckboxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
