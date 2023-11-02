493592b430cc1b59adabb754712379553d5e6919
saifmathur <saifmathur41@gmail.com>
2023-11-02 15:40:40 +0530

    added tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
+})
+export class NavbarComponent {
+  showMenu(){
+    let el = document.getElementById("userMenu")
+    let menuButton = document.getElementById("user-menu-button");
+    if(el?.classList.contains('hide')){
+      el.classList.remove('hide')
+      el.classList.add(
+        "show absolute bg-white focus:outline-none mt-2 origin-top-right py-1 right-0 ring-1 ring-black ring-opacity-5 rounded-md shadow-lg w-48 z-10 hide"
+      );
+      
+    }
+    else{
+      el?.classList.add("hide");
+      el?.classList.remove(
+        "show absolute bg-white focus:outline-none mt-2 origin-top-right py-1 right-0 ring-1 ring-black ring-opacity-5 rounded-md shadow-lg w-48 z-10 hide"
+      );
+      
+    }
+  }
+  // focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800
+}
