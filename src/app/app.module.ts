4bd6061bc14c95b933c0a2ea7ee81a10f16afeb0
saifmathur <saifmathur41@gmail.com>
2023-11-02 11:05:10 +0530

    fresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatBadgeModule } from "@angular/material/badge";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatCardModule } from "@angular/material/card";
 
-import { HeaderComponent } from "./components/header/header.component";
-import { HomeComponent } from "./pages/home/home.component";
-import { ProductsHeaderComponent } from './pages/home/home-child-components/products-header/products-header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    ProductsHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    MatBadgeModule,
     AppRoutingModule,
-    MatIconModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
