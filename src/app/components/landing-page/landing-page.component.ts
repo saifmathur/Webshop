7c049bc745546b27f59aad81d4b42878e65a7fb9
saifmathur <saifmathur41@gmail.com>
2023-11-08 19:12:24 +0530

    product cards

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  selector: "app-landing-page",
+  templateUrl: "./landing-page.component.html",
+  styleUrls: ["./landing-page.component.scss"],
 })
 export class LandingPageComponent {
+  featuredProducts: any = [
+    {
+      image:
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+      title: "Basic Tee",
+      desciption: "Black",
+      alt: "Front of men&#039;s Basic Tee in black.",
+      price: "35",
+      remainingQty: "2",
+    },
+    {
+      image: "",
+      title: "card 2",
+      desciption: "desc card 2",
+      price: "13",
+      alt: "Front of men&#039;s Basic Tee in black.",
+      remainingQty: "3",
+    },
+    {
+      image: "",
+      title: "card 3",
+      desciption: "desc card 3",
+      alt: "Front of men&#039;s Basic Tee in black.",
+      price: "14",
+      remainingQty: "4",
+    },
+  ];
 
+  showModal:boolean = false
+  openAddModal() {
+    
+    this.showModal = true
+    console.log("HERE", this.showModal);
+  }
 }
+
+
